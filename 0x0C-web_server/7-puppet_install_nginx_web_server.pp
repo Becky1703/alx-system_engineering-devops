3b159c4fcece91acde34e773764bbb861b937146
Becky1703 <oluwabukunmia@gmail.com>
2023-06-27 04:01:59 -0700

    web server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,35 @@
+#Installs and configures nginx using puppet
+
+#installs Nginx package
+package { 'nginx':
+  ensure => installed,
+}
+
+#Configuration for Nginx server
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0644',
+  content => "
+    server {
+      listen 80;
+      ubuntu /var/www/html;
+      
+      location / {
+        return 200 'Hello World!':
+      }
+
+      location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4/;
+      }
+    }
+  ",
+}
+
+#To restart Nginx service after configuration
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => File['/etc/nginx/sites-available/default'],
+}
