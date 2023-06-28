5543416bbc1368ec7432c89f6e794605aef5cbda
Becky1703 <oluwabukunmia@gmail.com>
2023-06-28 03:23:15 -0700

    error fix

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,32 +1,7 @@
 #Installs and configures nginx on Ubuntu using puppet
+#installs Nginx package and restarts after installation
 
-#installs Nginx package
-package { 'nginx':
-  ensure => installed,
-}
-
-#Configuration for Nginx server
-file { '/etc/nginx/sites-enabled/default':
-  ensure  => present,
-  content => @(EOF),
-    server {
-      listen 80;
-      root /var/www/html;
-      
-      location / {
-        return 200 'Hello World!':
-      }
-
-      location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4/;
-      }
-    }
- EOF
-}
-
-#To restart Nginx service after configuration
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  subscribe => File['/etc/nginx/sites-enabled/default'],
+exec { 'server configuration':
+  provider => shell,
+  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.html; sudo sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https:youtube.com;\\n\\t}\\n" /etc/nginx/sites-available/default; sudo service nginx restart',
 }
