0e976a0730c56a98ea236488b1f855a09b7bb7d4
Becky1703 <oluwabukunmia@gmail.com>
2023-08-16 12:31:58 +0000

    web stack debugging

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+#puppet manifest to congigure OS to allow a user login without errors
+exec { 'Change soft limit':
+  command  => 'sudo sed -i "s/holberton\ssoft.*/holberton\tsoft\tnofile\t10000/" /etc/security/limits.conf',
+  provider => shell,
+}
+
+exec { 'Change hard limit':
+  command  => 'sudo sed -i "s/holberton\shard.*/holberton\thard\tnofile\t100000/" /etc/security/limits.conf',
+  provider => shell,
+}
